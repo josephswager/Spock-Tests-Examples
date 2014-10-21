6846840773226ec384e507960ea4eaa45f9b7eab
Joseph Swager <josephswager@gmail.com>
2014-10-21 00:41:23 -0400

    cleanup on junit test

--- src/test/java/com/swager/MyMathTest.java
+++ src/test/java/com/swager/MyMathTest.java
@@ -20,46 +20,33 @@ public class MyMathTest {
 
     @Test
     public void testDoubleNumber() throws Exception {
-        // Arrange
         Integer numberExpected = new Integer(20);
 
-        // Act
         Integer numberDoubled = (Integer) math.doubleNumber(10);
 
-        // Assert
         assertEquals(numberExpected, numberDoubled);
     }
 
 
     @Test
     public void testDoubleNumber_expected_number_is_wrong_to_prove_method_works() throws Exception {
-        // Arrange
         Integer numberExpected = new Integer(100);
 
-        // Act
         Integer numberDoubled = (Integer) math.doubleNumber(10);
 
-        // Assert
         assertNotEquals(numberExpected, numberDoubled);
-        // numberDoubled is 20 not the mistaken 100 expected
     }
 
 
     @Test(expected = IllegalArgumentException.class)
     public void testDoubleNumber_passed_null_throws_illegalArgumentException() throws Exception {
-        // Act
         Integer numberDoubled = (Integer) math.doubleNumber(null);
-
-        // Assert
         assertNull(numberDoubled);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testDoubleNumber_passed_not_a_number_throws_illegalArgumentException() throws Exception {
-        // Act
         Integer numberDoubled = (Integer) math.doubleNumber("one");
-
-        // Assert
         assertNull(numberDoubled);
     }
 
