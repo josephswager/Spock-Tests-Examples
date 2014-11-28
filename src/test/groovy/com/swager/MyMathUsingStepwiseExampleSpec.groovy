091cbae8ef0a01f3dc38cfb58ef7e61eeff5aa27
Joseph Swager <josephswager@gmail.com>
2014-11-28 01:27:23 -0500

    added formatting to spock specifcation

--- src/test/groovy/com/swager/MyMathUsingStepwiseExampleSpec.groovy
+++ src/test/groovy/com/swager/MyMathUsingStepwiseExampleSpec.groovy
@@ -22,65 +22,64 @@ class MyMathUsingStepwiseExampleSpec extends Specification {
     @Shared
     def den = new BigDecimal(0)
 
-    def "Step 1: give the denominator a value of 5.5 then it den will be greater than zero and equal 5.5"() {
+    def "Step 1: "() {
 
-        when:
-        den = 5.5
+        when: "give the denominator a value of 5.5"
+            den = 5.5
 
-        then:
-        den > 0
-        den == 5.5
+        then: "then den will be greater than zero and equal 5.5"
+            den > 0
+            den == 5.5
     }
 
-    def "Step 2: double denominator then the den value will be 11 also we can seee the old method shows dens value is 5.5 before me made any changes"() {
+    def "Step 2: double denominator then the den value will be 11 also we can see the old method shows dens value is 5.5 before me made any changes"() {
         when:
-        den = math.doubleNumber(den)
+            den = math.doubleNumber(den)
 
         then:
-        5.5 == old(den)
-        den == 11
+            5.5 == old(den)
+            den == 11
 
     }
 
     def "Step 3: create the numerator with the given value 2 then its value will be greater than x=zero and equal to 2"() {
         when:
-        num = 2
+            num = 2
 
         then:
-        num > 0
-        num == 2
+            num > 0
+            num == 2
 
     }
 
     def "Step 4: when run prettyPrintPercentageOfDivision using #num as the numerator and #den as denominator the output will be a correctly formatted percentage to the second decimal and no exceptions will be thrown "() {
 
         when:
-        def percentage = math.prettyPrintPercentageOfDivision(num, den)
+            def percentage = math.prettyPrintPercentageOfDivision(num, den)
 
         then:
-        percentage == '18.18%'
-        notThrown IllegalArgumentException
+            percentage == '18.18%'
+            notThrown IllegalArgumentException
 
     }
 
     def "Step 5: when we set  denominator to 0 then it will equal zero and we can run the old method to see before we changed it wr had it set to 11"() {
         when:
-        den = 0
+            den = 0
 
         then:
-        11 == old(den)
-        den == 0
+            11 == old(den)
+            den == 0
 
     }
 
     def "Step 6: when we run prettyPrintPercentageOfDivision using #num as the numerator and #den as denominator throws an illegalArgumentException because we looking for divisors of zero which would cause and Arithmetic exception so we check this and then throw the exception already stated with the message 'Argument 'divisor' is 0'"() {
         when:
-        def percentage = math.prettyPrintPercentageOfDivision(num, den)
+            def percentage = math.prettyPrintPercentageOfDivision(num, den)
 
         then:
-        IllegalArgumentException ex = thrown()
-
-        ex.message == "Argument 'divisor' is 0"
+            IllegalArgumentException ex = thrown()
+            ex.message == "Argument 'divisor' is 0"
     }
 
 }
